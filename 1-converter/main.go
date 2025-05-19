b1033ca78ac5931c178f7753b5a94c603f9d412e
Viktor Shishkalov <viktorshish@gmail.com>
2025-05-19 11:18:02 +0300

    Add  function user input

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -1,7 +1,23 @@
 package main
 
+import "fmt"
+
 func main() {
 	const USD_EUR = 0.9
 	const USD_RUB = 80
 	const EUR_RUB = 91
 }
+
+func getUserValues() (float64, string, string) {
+	var amount float64
+	var sourceСurrency, targetСurrency string
+	fmt.Print("Введите сколько хотите поменять")
+	fmt.Scan(&amount)
+	fmt.Print("Введите какую валюту хотите поменять (USD, EUR, RUB)")
+	fmt.Scan(&sourceСurrency)
+	fmt.Print("Введите какую валюту хотите получить после обмена (USD, EUR, RUB)")
+	fmt.Scan(&targetСurrency)
+	return amount, sourceСurrency, targetСurrency
+}
+
+func changeMoney(amount float64, sourceСurrency string, targetСurrency float64) float64 {}
